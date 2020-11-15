663ff025738dfcdd58bb2607f1d23d62af7dc760
pawewor862 <pawewor862@student.polsl.pl>
2020-11-15 23:19:37 +0100

    Add model Covid

---
+++ UI/Assets/Scripts/Models/CovidModel.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CovidModel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool Infected { get; set; }
+
+    public GameObject AnotherAgent;
+    public GameObject ThisAgent;
+   
+
+    private System.Random rand;
+    private float weightr;
+    private float weighthealth;
+    private float weightmask;
+    private float rsphare;
+    private Helath health;
+
+    private float _probability;
+    public float Probability { 
+        get { return _probability; }
+        set
+        {
+            if (value >= 1f)
+                _probability = 1f;
+            else if (value <= 0f)
+                _probability = 0f;
+            else
+                _probability = value;
+        }
+    }
+
+    public CovidModel(GameObject AnotherAgent, GameObject ThisAgent, float weightr, float weighthealth, float weightmask, float rsphare)
+    {
+        this.AnotherAgent = AnotherAgent;
+        this.ThisAgent = ThisAgent;
+        this.weightr = weightr;
+        this.weighthealth = weighthealth;
+        this.weightmask = weightmask;
+        this.rsphare = rsphare;
+        rand = new System.Random();
+        health = ThisAgent.GetComponent<Helath>();
+    }
+
+    private bool CalculatingProbabilities()
+    {
+        return (int)(Probability*100) <= rand.Next(0, 100);
+    }
+
+    void ChanceINfacted()
+    {
+        float BoolMask;
+        if (AnotherAgent.GetComponent<Mask>().BoolMask)
+            BoolMask = 1.0f;
+        else
+            BoolMask = 0.0f;
+
+        float r = rsphare - Mathf.Sqrt(Mathf.Pow(ThisAgent.transform.position.x - AnotherAgent.transform.position.x, 2) + Mathf.Pow(ThisAgent.transform.position.y - AnotherAgent.transform.position.y, 2) + Mathf.Pow(ThisAgent.transform.position.z - AnotherAgent.transform.position.z, 2));
+        Probability = Probability + (r*weightr + (1-health.Value)*weighthealth - 0.5f* weightmask* BoolMask) * Time.deltaTime;
+
+
+        // Zrobic warunek w ktorym co sekunde zostaje przeliczone prawdopodobiienstwo zakazenia
+    }
+
+}
---
+++ UI/Assets/Scripts/MonoBehaviours/Mask.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mask : MonoBehaviour
+{
+    public bool BoolMask;
+
+
+
+    // The concept is to put on a physical mask
+    void PutTakeMask()
+    {
+        if (BoolMask)
+            BoolMask = false;
+        else
+            BoolMask = true;
+    }
+}
