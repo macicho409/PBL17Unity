c1a38c46df8fd1e4e1bd61166c8806292ddddd57
lombo-cmyk <srsrsrew@gmail.com>
2020-12-09 21:00:17 +0100

    more code thrown away

--- UI/Assets/Scripts/MonoBehaviours/ThirdPersonUserControl.cs
+++ UI/Assets/Scripts/MonoBehaviours/ThirdPersonUserControl.cs
@@ -12,31 +12,15 @@ namespace Assets.ThirdPerson
     public class ThirdPersonUserControl : MonoBehaviour
     {
         private ThirdPersonCharacter m_Character; // A reference to the ThirdPersonCharacter on the object
-        private Transform m_Cam;                  // A reference to the main camera in the scenes transform
         public Transform target;
         private PhysiologicalModel m_Model;
         NavMeshAgent agent;
-        private Vector3 m_CamForward;             // The current forward direction of the camera
-        private Vector3 m_Move;
         private bool m_Jump;
         private Vector3 destination;
         private float timeSinceUpdate = 0;
-        private bool simStart = true;
 
         private void Start()
         {
-            // get the transform of the main camera
-            if (Camera.main != null)
-            {
-                m_Cam = Camera.main.transform;
-            }
-            else
-            {
-                Debug.LogWarning(
-                    "Warning: no main camera found. Third person character needs a Camera tagged \"MainCamera\", for camera-relative controls.", gameObject);
-                // we use self-relative controls in this case, which probably isn't what the user wants, but hey, we warned them!
-            }
-
             // get the third person character ( this should never be null due to require component )
             agent = GetComponent<NavMeshAgent>();
             m_Character = GetComponent<ThirdPersonCharacter>();
@@ -54,30 +38,21 @@ namespace Assets.ThirdPerson
             }
         }
 
-
         // Fixed update is called in sync with physics
         private void FixedUpdate()
         {
-            // calculate move direction to pass to character
-            if (m_Cam != null)
-            {
-                // calculate camera relative direction to move:
-                m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
-                //m_Move = v*m_CamForward + h*m_Cam.right;
-            }
-
             if (agent.remainingDistance < 0.001f && Time.time - timeSinceUpdate > 1)
             {
-                Debug.Log("First if");
+                Debug.Log("Stoping");
                 timeSinceUpdate = Time.time;
                 m_Character.ChangeAnimatorState(0.0f);
-                //position.RemoveAt(0);
-                agent.SetDestination(FindDestination());
+                destination = FindDestination();
+                agent.SetDestination(destination);
                 agent.isStopped = true;
             }
             if (Time.time - timeSinceUpdate > 5 && agent.isStopped == true)
             {
-                Debug.Log("Second if");
+                Debug.Log("Moving");
                 agent.isStopped = false;
                 m_Character.ChangeAnimatorState(0.4f);
             }
