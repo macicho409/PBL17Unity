ba01ff32e7915a1ed88e3f50e4ff279accca9202
oszczep <macicho409@student.polsl.pl>
2021-02-02 10:08:08 +0100

    Add start values to StaticContainerService.cs

--- UI/Assets/Scripts/Services/StaticContainerService.cs
+++ UI/Assets/Scripts/Services/StaticContainerService.cs
@@ -8,16 +8,16 @@ namespace Assets.Scripts.Services
 {
     public static class StaticContainerService
     {
-        public static int NoAgents = 1;
+        public static int NoAgents = 50;
 
-        public static float WeightTimeSleep = 0.01f;
-        public static float WeightTimeFood = 0.01f;
-        public static float WeightTimeWater = 0.01f;
-        public static float WeightTimeToilet = 0.01f;
-        public static float WeightTimeSex = 0.01f;
-        public static float WeightTimeHigerNeeds = 0.01f;
+        public static float WeightTimeSleep = 0.0025f;
+        public static float WeightTimeFood = 0.0025f;
+        public static float WeightTimeWater = 0.0025f;
+        public static float WeightTimeToilet = 0.0025f;
+        public static float WeightTimeSex = 0.0025f;
+        public static float WeightTimeHigerNeeds = 0.0025f;
 
-        public static int SampleTimeCovid = 1;
+        public static int SampleTimeCovid = 10;
 
         public static int CovidDelay = 30;
     }
